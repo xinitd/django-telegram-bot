32a99592912cb16ee300bd3ec45425e068a91acd
Shakhzhakhan Maxudbek <xinit@args.tech>
2024-07-24 18:44:25 +0500

    Init

---
+++ core/bot.py
@@ -0,0 +1,18 @@
+import telebot
+from backend.settings import settings
+
+
+bot = telebot.TeleBot(settings['TELEGRAM_TOKEN'])
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+# bot.infinity_polling()
